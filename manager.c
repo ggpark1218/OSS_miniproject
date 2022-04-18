e628039b7f8738dbd138a72b628bd500e015dbbc
ggpark1218 <rbrud1218@gmail.com>
2022-04-19 01:07:33 +0900

    make searchProduct

--- manager.c
+++ manager.c
@@ -10,3 +10,28 @@ void listProduct(Product *p[], int count){
     }
     printf("\n");
 }      // 전체 등록된 제품 리스트 출력
+
+void searchProduct(Product *p[], int count){
+    int scnt = 0;
+    char search[20];
+
+    printf("검색할 이름은? ");
+    scanf(" %s", search);
+
+    printf("*****************\n");
+
+    for(int i=0; i<count; i++){
+       if(p[i]->price == -1) continue;
+            if(strstr(p[i]->product_name, search)){
+                printf("%2d ", i+1);
+                readScore(p[i]);
+                scnt++;
+            }
+    }
+
+    if(scnt == 0) {
+        printf("=>검색된 데이터 없음!");
+    }
+    printf("\n");
+
+}	//제품 이름 검색
