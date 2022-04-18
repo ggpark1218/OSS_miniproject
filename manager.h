38656e8e99f7fbafc2c261ffa31ea998aa93fac9
ggpark1218 <rbrud1218@gmail.com>
2022-04-19 01:26:04 +0900

    define searchPrice

--- manager.h
+++ manager.h
@@ -3,3 +3,4 @@
 void listProduct(Product *p[], int count);      // 전체 등록된 제품 리스트 출력
 void searchProduct(Product *p[], int count);    // 제품이름 검색
 void searchDetail(Product *p[], int count);     // 제품설명 검색
+void searchPrice(Product *p[], int count);      // 가격 범위 검색
