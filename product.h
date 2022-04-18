0976384195ddd13ae5426f45f14ac0b61217de1c
ggpark1218 <rbrud1218@gmail.com>
2022-04-19 01:50:19 +0900

    update product.h

--- product.h
+++ product.h
@@ -17,5 +17,13 @@ int createProduct(Product *p);                  // 제품을 추가하는 함수
 void readProduct(Product p);                    // 하나의 제품 출력 함수
 int updateProduct(Product *p);                  // 이미 추가된 제품을 수정하는 함수
 int deleteProduct(Product *p[], int count);     // 제품 삭제를 하기 위한 함수
+
+void searchProduct(Product *p[], int count);    // 제품이름 검색
+void searchDetail(Product *p[], int count);     // 제품설명 검색
+void searchPrice(Product *p[], int count);      // 가격 범위 검색
+
+void saveData(Product *p[], int count);         // 파일로 저장
+int loadData(Product *p[]);                     // 파일 불러오기
+
 void listProduct(Product *p[], int count);      // 전체 등록된 제품 리스트 출력
 int selectMenu();   //메뉴 선택
